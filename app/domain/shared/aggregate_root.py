addac3468668c1f753b4149fd4e3fd6883e926f6
KhalidLumbaLumba <kakadiqi06@gmail.com>
2026-06-06 12:19:20 +0700

    Fea(test): Booking Fixing

--- app/domain/shared/aggregate_root.py
+++ app/domain/shared/aggregate_root.py
@@ -8,6 +8,11 @@ class AggregateRoot:
     def add_domain_event(self, event: DomainEvent) -> None:
         self._domain_events.append(event)
 
+    def collect_events(self) -> List[DomainEvent]:
+        events = self._domain_events.copy()
+        self.clear_domain_events()
+        return events
+
     def clear_domain_events(self):
         self._domain_events.clear()
 
